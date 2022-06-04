aebf1a10d7b9f418ceffe159bb8c78957ec36167
Geetha Kondamuri <kondamurigeetha123@gmail.com>
2022-06-04 11:25:06 +0530

    Creating frontend and installing nginx

--- Roboshop.sh/components/frontend.sh
+++ Roboshop.sh/components/frontend.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
+source components/commmon.sh
+
+## Installing nginx
+
 echo "installing nginx"
-sudo yum install nginx -y
+sudo yum install nginx -y &>>LOG_FILE
+
+## downloading frontend content
+echo "downloaded the content"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>LOG_FILE
+
+##cleaning the content
+echo "cleaning the content"
+rm -rf /usr/share/nginx/html/* &>>LOG_FILE
+
+## Extract frontend content
+echo "unzipping the file"
+cd /tmp
+unzip -o frontend.zip &>>LOG_FILE
+
+##copy extracted file
+###echo "copying the unzipped file"

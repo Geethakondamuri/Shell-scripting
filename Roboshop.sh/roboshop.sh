f00f423162689ffd34000e8fadb4b837a049d819
Geetha Kondamuri <kondamurigeetha123@gmail.com>
2022-06-14 19:50:50 +0530

    Creating roboshop

--- Roboshop.sh/roboshop.sh
+++ Roboshop.sh/roboshop.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-components/$1
+if [ -f components/$1.sh ];then
+  bash components/$1.sh
+else
+  echo -e "\e[1;31mInvalid Inputs\[0m"
+  echo -e "\e[1;32mAvailable Inputs are frontend|mongodb|catalogue|redis|user|cart|mysql|shipping|payment|rabbitmq|dispatch\e[0m"
+fi

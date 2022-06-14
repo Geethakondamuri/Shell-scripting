87dbed017d908b7261ae69e4f94c8b30b44b86aa
Geetha Kondamuri <kondamurigeetha123@gmail.com>
2022-06-14 21:09:58 +0530

    Created function and declared in scripts to run

--- Roboshop.sh/components/common.sh
+++ Roboshop.sh/components/common.sh
@@ -5,9 +5,9 @@
 ## Declaring a function
 STAT(){
   if [ $1 -eq 0 ]; then
-    echo "\e[1;31mSUCCESS\[0m"
+    echo "\e[1;31mSUCCESS\e[0m"
   else
-    echo "\e[1;31mFAILED\[0m"
+    echo "\e[1;31mFAILED\e[0m"
     exit 2
   fi
 }
